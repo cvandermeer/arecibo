b1f435ab38fd057ec44bcf31f688457ffd012d43
Chris van der Meer <cvandermeer@live.nl>
2015-09-12 13:42:13 +0200

    generate random planet name

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -2,4 +2,9 @@ class PlanetsController < ApplicationController
   def index
     @planets = Planet.all
   end
+
+  def create
+    @planet = Planet.new
+    redirect_to planets_path if @planet.save
+  end
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,14 @@
 class Planet < ActiveRecord::Base
+  ### CALLBACKS ###
+  before_create :generate_planet
+
+  def generate_planet
+    a = ('a'..'z').to_a.sample
+    b = ('a'..'z').to_a.sample
+    c = ('a'..'z').to_a.sample
+    d = ('0'..'9').to_a.sample
+    e = ('0'..'9').to_a.sample
+    f = ('0'..'9').to_a.sample
+    self.name = a + b + c + '-' + d + e + f
+  end
 end
