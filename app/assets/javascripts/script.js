24deb0f22265568e9af6988e364e924c208d8bbe
Chris van der Meer <cvandermeer@live.nl>
2015-09-12 13:29:11 +0200

    resetting some styling

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,32 +1,6 @@
 var ready;
 ready = function() {
 
- var right = false
- $('.next').on('click', function(){
-    if(right == false){
-      $('.specie').find('img').animate({
-        left: '-=200'
-      });
-      $('.specie').find('.drogan').fadeOut(900, function(){
-        $('.specie').find('.cynode').fadeIn(900);
-      });
-
-      right = true
-    }
- });
-
- $('.previous').on('click', function(){
-    if(right == true){
-      $('.specie').find('img').animate({
-        left: '+=200'
-      });
-      $('.specie').find('.cynode').fadeOut(900, function(){
-        $('.specie').find('.drogan').fadeIn(900);
-      });
-      right = false
-    }
- })
-
 }
 
 $(document).ready(ready);
